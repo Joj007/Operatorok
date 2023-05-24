0e33a997107a5c23c30fa198bca48c2c615760c2
sinka <sinka@DESKTOP-7EN5LQP>
2023-05-24 14:37:16 +0200

    Add project files.

---
+++ Operatorok/Operátor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    internal class Operátor
+    {
+        int eloTag;
+        string muveletiJel;
+        int utoTag;
+
+        public Operátor(int eloTag, string jel, int utoTag)
+        {
+            this.eloTag = eloTag;
+            this.muveletiJel = jel;
+            this.utoTag = utoTag;
+        }
+
+        public static string Kiszamol(int eloTag, string muveletiJel, int utoTag)
+        {
+            string eredmeny = "";
+            try
+            {
+                switch (muveletiJel)
+                {
+                    case "+":
+                        eredmeny = $"{eloTag + utoTag}";
+                        break;
+                    case "-":
+                        eredmeny = $"{eloTag - utoTag}";
+                        break;
+                    case "*":
+                        eredmeny = $"{eloTag * utoTag}";
+                        break;
+                    case "/":
+                        eredmeny = $"{eloTag / (double)utoTag}";
+                        break;
+                    case "div":
+                        eredmeny = $"{eloTag / utoTag}";
+                        break;
+                    case "default":
+                        eredmeny = "Hibás operátor!";
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                eredmeny = "Egyéb hiba!";
+            }
+
+            return eredmeny;
+        }
+
+        public int EloTag { get => eloTag;}
+        public string MuveletiJel { get => muveletiJel;}
+        public int UtoTag { get => utoTag;}
+    }
+}
---
+++ Operatorok/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Operatorok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Operátor> operatorok = new List<Operátor> ();
+            foreach (string sor in File.ReadLines("szoveg.txt"))
+            {
+                string[] adatok = sor.Split();
+                operatorok.Add(new Operátor(int.Parse(adatok[0]), adatok[1], int.Parse(adatok[2])));
+            }
+
+            Console.WriteLine($"2. feladat: Kifejezések száma: {operatorok.Count()}");
+
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {operatorok.Count(n=>n.MuveletiJel=="mod")}");
+
+            Console.Write("4. feladat: ");
+            Console.WriteLine(operatorok.Any(n => n.EloTag % 10 == 0 && n.UtoTag % 10 == 0) ? "Van ilyen kifejezés!" : "Nincs ilyen kifejezés!");
+
+            Console.WriteLine("5. feladat: Statisztika");
+            string[] megfeleloOperatorok = {"mod", "/", "div", "-", "*", "+"};
+            operatorok.Where(n=>megfeleloOperatorok.Contains(n.MuveletiJel)).GroupBy(n => n.MuveletiJel).ToList().ForEach(n=>Console.WriteLine($"\t{n.Key} -> {n.Count()} db"));
+
+            Console.Write("7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+            string bekeres = Console.ReadLine();
+            string[] bekertAdatok;
+            while (bekeres!="vége"){
+                bekertAdatok = bekeres.Split();
+                try
+                {
+                    Console.WriteLine($"\t{bekeres} = {Operátor.Kiszamol(int.Parse(bekertAdatok[0]), bekertAdatok[1], int.Parse(bekertAdatok[2]))}");
+                }
+                catch (Exception)
+                {
+                    Console.Write("7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+                    bekeres = Console.ReadLine();
+                    continue;
+                }
+                Console.Write("7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+                bekeres = Console.ReadLine();
+            };
+
+
+            operatorok.ForEach(n => File.WriteAllText("eredmenyek.txt", $"{n.EloTag} {n.MuveletiJel} {n.UtoTag} = {Operátor.Kiszamol(n.EloTag, n.MuveletiJel, n.UtoTag)}"));
+
+            
+
+
+        }
+    }
+}
